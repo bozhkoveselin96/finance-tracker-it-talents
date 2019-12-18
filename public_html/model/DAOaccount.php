06821b21cc44c58a9c699547ca0f71126e12f580
bozhkoveselin <bozhkoveselin@gmail.com>
2019-12-18 16:13:00 +0200

    added DAOaccount

---
+++ public_html/model/DAOaccount.php
@@ -0,0 +1,20 @@
+<?php
+require_once "DBconnection.php";
+
+function createAccount(array $account) {
+    try {
+        $data = [];
+        $data["name"] = $account["name"];
+        $data["current_amount"] = $account["current_name"];
+        $data["owner_id"] = $account["owner_id"];
+
+        $conn = getPDO();
+        $sql = "INSERT INTO accounts(name, current_amount, owner_id, date_created)
+                VALUES (?, ?, ?, CURRENT_DATE);";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($data);
+        return true;
+    } catch (PDOException $exception) {
+        return false;
+    }
+}
