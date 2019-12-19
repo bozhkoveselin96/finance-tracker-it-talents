1d56a575f4dd5021fdc93c22438ea189bb5c099e
bozhkoveselin <bozhkoveselin@gmail.com>
2019-12-19 11:47:40 +0200

    added delete backend validation

---
+++ public_html/controller/account_controller/delete.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once "../../model/DAOuser.php";
+
+if (isset($_POST["delete"])) {
+    $user_id = $_POST["user_id"];
+    $account_id = $_POST["account_id"];
+    $account = getAccountById($account_id);
+    $response["status"] = false;
+
+    if ($user_id == $_SESSION["logged_user"] &&
+        $account["owner_id"] == $user_id &&
+        deleteAccount($account_id)) {
+        $response["status"] = true;
+    }
+    echo json_encode($response);
+}
