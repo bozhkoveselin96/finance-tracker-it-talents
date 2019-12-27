489dbe5182e2e2dcf2f792e48a687aa5aa798b03
Emil Raykov <emilraykov@outlook.com>
2019-12-27 13:34:04 +0200

    Made validator class

--- public_html/app/controller/AccountController.php
+++ public_html/app/controller/AccountController.php
@@ -13,9 +13,7 @@ class AccountController {
         $response['status'] = false;
         if (isset($_POST['add_account']) && isset($_SESSION['logged_user'])) {
             $account = new Account($_POST['name'], $_POST['current_amount'], $_SESSION['logged_user']);
-
-            if (mb_strlen($account->getName()) >= MIN_LENGTH_NAME && is_numeric($account->getCurrentAmount()) &&
-                $account->getCurrentAmount() >= 0) {
+            if (Validator::validateName($account->getName()) && Validator::validateAmount($account->getCurrentAmount())) {
                 if (AccountDAO::createAccount($account)) {
                     $response['status'] = true;
                     $response['target'] = 'addaccount';
@@ -30,7 +28,7 @@ class AccountController {
         $response['status'] = false;
         if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_SESSION['logged_user']) && isset($_GET['user_id'])) {
             $user_id = $_GET['user_id'];
-            if ($user_id == $_SESSION['logged_user']) {
+            if (Validator::validateLoggedUser($user_id)) {
                 $accounts = AccountDAO::getMyAccounts($user_id);
                 if ($accounts) {
                     $response['status'] = true;
@@ -46,11 +44,10 @@ class AccountController {
         if (isset($_POST["edit"])) {
             $account_id = $_POST["account_id"];
             $account = AccountDAO::getAccountById($account_id);
-            if ($account) {
+            if ($account && Validator::validateLoggedUser($_POST["user_id"])) {
                 $newAccountName = new Account($_POST["name"], $account->current_amount, $_POST["user_id"]);
                 $newAccountName->setId($account_id);
-                if ($newAccountName->getOwnerId() == $_SESSION["logged_user"] &&
-                    $newAccountName->getOwnerId() == $account->owner_id &&
+                if ($newAccountName->getOwnerId() == $account->owner_id &&
                     AccountDAO::editAccount($newAccountName)) {
                     $response["status"] = true;
                 }
@@ -66,7 +63,7 @@ class AccountController {
             $account_id = $_POST["account_id"];
             $account = AccountDAO::getAccountById($account_id);
 
-            if ($account && $user_id && $user_id == $_SESSION["logged_user"] &&
+            if ($account && $user_id && Validator::validateLoggedUser($user_id) &&
                 $user_id == $account->owner_id && AccountDAO::deleteAccount($account_id)) {
                 $response["status"] = true;
             }
