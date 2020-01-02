820f8656e52e30b6aabaaabdd420e55de1c63024
bozhkoveselin <bozhkoveselin@gmail.com>
2020-01-02 16:27:31 +0200

    added some methods in StatisticDAO

--- public_html/app/model/StatisticDAO.php
+++ public_html/app/model/StatisticDAO.php
@@ -38,4 +38,52 @@ class StatisticDAO {
             return false;
         }
     }
+
+    public static function getTrForSelectedPeriod($user_id, $type, $from_date, $to_date) {
+        try {
+            $conn = Connection::get();
+            $sql = "SELECT COALESCE(SUM(t.amount), 0) AS money FROM transactions AS t
+                    JOIN accounts AS a ON(a.id = t.account_id)
+                    JOIN transaction_categories AS tc ON(t.category_id = tc.id)
+                    WHERE a.owner_id = ? AND tc.type = ? AND t.time_event BETWEEN ? AND ?;";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$user_id, $type, $from_date, $to_date]);
+            return $stmt->fetch(\PDO::FETCH_OBJ);
+        } catch (\PDOException $exception) {
+            return false;
+        }
+    }
+
+    public static function getTrForSelectedPeriodByCategories($user_id, $type, $from_date, $to_date) {
+        try {
+            $conn = Connection::get();
+            $sql = "SELECT tc.name AS category_name,
+                    COALESCE(SUM(t.amount), 0) AS money FROM transactions AS t
+                    JOIN accounts AS a ON(a.id = t.account_id)
+                    JOIN transaction_categories AS tc ON(t.category_id = tc.id)
+                    WHERE a.owner_id = ? AND tc.type = ? AND t.time_event BETWEEN ? AND ?
+                    GROUP BY t.category_id;";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$user_id, $type, $from_date, $to_date]);
+            return $stmt->fetchAll(\PDO::FETCH_OBJ);
+        } catch (\PDOException $exception) {
+            return false;
+        }
+    }
+
+    public static function getBudgetProgress($budget_id, $owner_id) {
+        try {
+            $conn = Connection::get();
+            $sql = "SELECT (b.amount-SUM(t.amount)) AS remainder, SUM(t.amount) AS spent_so_far
+                    FROM budgets AS b
+                    JOIN transactions AS t ON(b.category_id = t.category_id)
+                    WHERE t.time_event BETWEEN b.from_date AND b.to_date 
+                    AND b.id = ? AND b.user_id = ?;";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$budget_id, $owner_id]);
+            return $stmt->fetch(\PDO::FETCH_OBJ);
+        } catch (\PDOException $exception) {
+            return false;
+        }
+    }
 }
