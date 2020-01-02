2d04688ffef88094d41547e897c69e3042f2d5c2
Emil Raykov <emilraykov@outlook.com>
2020-01-02 14:29:33 +0200

    Added StatisticController

---
+++ public_html/app/controller/StatisticController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace controller;
+
+
+use model\StatisticDAO;
+
+class StatisticController {
+    public function getIncomesOutcomes() {
+        $status = STATUS_BAD_REQUEST . 'You do not have transactions or you are not logged in.';
+        $response = [];
+        if (isset($_SESSION['logged_user'])) {
+            $incomes = StatisticDAO::getTransactionsSum($_SESSION['logged_user'], 1);
+            $outcomes = StatisticDAO::getTransactionsSum($_SESSION['logged_user'], 0);
+            if ($incomes && $outcomes) {
+                $response[] = $incomes;
+                $response[] = $outcomes;
+                $status = STATUS_OK;
+            }
+        }
+        header($status);
+        return $response;
+    }
+}
